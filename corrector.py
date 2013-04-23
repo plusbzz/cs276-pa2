56163f00a03e61f5aea09d05ec5b28638329f4b9
Raj Bandyopadhyay <plusbzz@gmail.com>
2013-04-22 22:57:48 -0400

    Add jaccard calculation

--- corrector.py
+++ corrector.py
@@ -7,6 +7,14 @@ google_loc = 'data/google.txt'
 
 alphabet = "abcdefghijklmnopqrstuvwxyz0123546789&$+_' "
 
+
+def jaccard_coeff(s1,s2):
+  s1 = set([(t1+t2) for t1,t2 in zip(s1[:-1],s1[1:])])
+  s2 = set([(t1+t2) for t1,t2 in zip(s2[:-1],s2[1:])])
+  
+  return (1.0*len(s1.intersection(s2)))/len(s1.union(s2))
+
+
 def read_query_data():
   """
   all three files match with corresponding queries on each line
